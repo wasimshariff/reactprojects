8b1c59e07aeeda1eb640672b4c5d320adf082847
Wasim Shariff <wasim.shariff@physiciansmutual.com>
2022-09-06 17:53:58 -0500

    pushing changes

---
+++ css_project_module_6/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+
+  const goalInputChangeHandler = (event) => {
+    setEnteredValue(event.target.value);
+  };
+
+  const testOnClickHandler = (event) => {
+    console.log(event);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </div>
+      <Button className="helloWsa" type="submit" onClick={testOnClickHandler}>
+        Add Goal
+      </Button>
+    </form>
+  );
+};
+
+export default CourseInput;
